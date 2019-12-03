02c45088f594b23016fefedb532d24db1da1a5cb
林威震 <weizhen19990727@gmail.com>
2019-12-03 21:59:16 +0800

    添加home的路由並且新增homecontroller

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', 'HomeController@index');
 // 認證路由...
 Route::auth();
 //對應TASK控制器的路由
 Route::get('/tasks', 'TaskController@index');
 Route::post('/task', 'TaskController@store');
 Route::delete('/task/{task}', 'TaskController@destroy');
+
